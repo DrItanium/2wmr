07e2831816fcc30d49b182a00d1d6430c02a2733
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-04-08 21:04:32 -0700

    Rewrite xerror

--- main.cc
+++ main.cc
@@ -114,6 +114,10 @@ quit(Arg *arg) {
 	running = False;
 }
 
+inline bool checkRequestAndErrorCode(XErrorEvent* ee, decltype(ee->request_code) rcode, decltype(ee->error_code) ecode) {
+    return (ee->request_code == rcode) && (ee->error_code == ecode);
+}
+
 /* There's no sy to check accesses to destroyed windows, thus those cases are
  * ignored (especially on UnmapNotify's).  Other types of errors call Xlibs
  * default error handler, which may call exit.
@@ -121,16 +125,16 @@ quit(Arg *arg) {
 int
 xerror(Display *dpy, XErrorEvent *ee) {
 	if(ee->error_code == BadWindow
-	|| (ee->request_code == X_SetInputFocus && ee->error_code == BadMatch)
-	|| (ee->request_code == X_PolyText8 && ee->error_code == BadDrawable)
-	|| (ee->request_code == X_PolyFillRectangle && ee->error_code == BadDrawable)
-	|| (ee->request_code == X_PolySegment && ee->error_code == BadDrawable)
-	|| (ee->request_code == X_ConfigureWindow && ee->error_code == BadMatch)
-	|| (ee->request_code == X_GrabKey && ee->error_code == BadAccess)
-	|| (ee->request_code == X_CopyArea && ee->error_code == BadDrawable))
+            || checkRequestAndErrorCode(ee, X_SetInputFocus, BadMatch)
+            || checkRequestAndErrorCode(ee, X_PolyText8, BadDrawable)
+            || checkRequestAndErrorCode(ee, X_PolyFillRectangle, BadDrawable)
+            || checkRequestAndErrorCode(ee, X_PolySegment, BadDrawable)
+            || checkRequestAndErrorCode(ee, X_ConfigureWindow, BadMatch)
+            || checkRequestAndErrorCode(ee, X_GrabKey, BadAccess)
+            || checkRequestAndErrorCode(ee, X_CopyArea, BadDrawable)) {
 		return 0;
-	fprintf(stderr, "2wm: fatal error: request code=%d, error code=%d\n",
-		ee->request_code, ee->error_code);
+    }
+    print(std::cerr, "2wm: fatal error: request code=", ee->request_code, ", error code=", ee->error_code, "\n");
 	return xerrorxlib(dpy, ee); /* may call exit */
 }
 
